d23dd85b0cf45cf8f4976cca62279ecac851d87c
kbpop <karl.phillip.petzold.bradley@gmail.com>
2026-05-25 20:50:53 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -104,6 +104,7 @@ func main() {
 	}
 
 	isolateFs(jailpath, command)
+	printPid()
 
 	cmd.Stdout = os.Stdout // create standard output for child process to talk through
 	cmd.Stderr = os.Stderr // create error output for child process to talk through
