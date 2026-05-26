e0f45e9bcb33abef8fa2fd419397b22f483b338b
kbpop <karl.phillip.petzold.bradley@gmail.com>
2026-05-25 22:14:33 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -111,7 +111,7 @@ func parentMode(){
 	childArgs := append([]string{"child"}, args...)
 	cmd := exec.Command("/proc/self/exe", childArgs...)	
 
-	println("command: %s", args[2])
+	println("command: %s", childArgs[2])
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
